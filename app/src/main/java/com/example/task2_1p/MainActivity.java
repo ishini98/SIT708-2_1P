3c33ba011eac60aa73b1bf98150b4aa8aec1fde1
Ishini Bhagya <59154447+ishini98@users.noreply.github.com>
2024-04-12 15:42:39 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/task2_1p/MainActivity.java
+++ app/src/main/java/com/example/task2_1p/MainActivity.java
@@ -1,91 +1,46 @@
-package com.example.task2_1p;
+package com.example.unitconverter;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.DecimalFormat;
-
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner sourceUnitSpinner, destinationUnitSpinner, typeSpinner;
-    private EditText sourceInput;
-    private Button convertButton;
+    private Spinner categorySpinner, sourceUnitSpinner, destinationUnitSpinner;
+    private EditText inputValueEditText;
     private TextView resultTextView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
+        categorySpinner = findViewById(R.id.categorySpinner);
+        sourceUnitSpinner = findViewById(R.id.sourceUnitSpinner);
+        destinationUnitSpinner = findViewById(R.id.destinationUnitSpinner);
+        inputValueEditText = findViewById(R.id.inputValueEditText);
+        android.widget.Button convertButton = findViewById(R.id.convertButton);
+        resultTextView = findViewById(R.id.resultTextView);
 
-        sourceUnitSpinner = findViewById(R.id.sourceUnit);
-        destinationUnitSpinner = findViewById(R.id.destinationUnit);
-        typeSpinner = findViewById(R.id.type);
-        sourceInput = findViewById(R.id.sourceInput);
-        convertButton = findViewById(R.id.Convert);
-        resultTextView = findViewById(R.id.result);
+        ArrayAdapter<CharSequence> categoryAdapter = ArrayAdapter.createFromResource(this,
+                R.array.conversion_categories, android.R.layout.simple_spinner_item);
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(categoryAdapter);
 
-        // Clear result text view
-        resultTextView.setText("");
-
-        // Initialize spinner adapter for conversion types
-        ArrayAdapter<CharSequence> typeAdapter = ArrayAdapter.createFromResource(this,
-                R.array.conversion_types, android.R.layout.simple_spinner_item);
-        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        typeSpinner.setAdapter(typeAdapter);
-
-        // Set item selected listener for conversion types spinner
-        typeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedType = parent.getItemAtPosition(position).toString();
-                if (selectedType.equals("Length") || selectedType.equals("Weight") || selectedType.equals("Temperature")) {
-                    sourceInput.setEnabled(true);
-                } else {
-                    sourceInput.setEnabled(false);
-                }
-
-                // Retrieve unit arrays from resources
-                String[] lengthUnits = getResources().getStringArray(R.array.length_units);
-                String[] weightUnits = getResources().getStringArray(R.array.weight_units);
-                String[] temperatureUnits = getResources().getStringArray(R.array.temperature_units);
-
-                // Determine unit array based on selected type
-                String[] units;
-                if (selectedType.equals("Length")) {
-                    units = lengthUnits;
-                } else if (selectedType.equals("Weight")) {
-                    units = weightUnits;
-                } else {
-                    units = temperatureUnits;
-                }
-
-                // Set adapter for source and destination unit spinners
-                ArrayAdapter<String> unitAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, units);
-                unitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                sourceUnitSpinner.setAdapter(unitAdapter);
-                destinationUnitSpinner.setAdapter(unitAdapter);
+                String selectedCategory = parent.getItemAtPosition(position).toString();
+                updateUnitSpinners(selectedCategory);
             }
 
             @Override
@@ -94,52 +49,55 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        convertButton.setOnClickListener(v -> {
+            String inputText = inputValueEditText.getText().toString().trim();
 
-        convertButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // get EditText content
-                String inputText = sourceInput.getText().toString().trim();
-
-                // check if null
-                if (inputText.isEmpty() || !isValidDouble(inputText)) {
-                    // if null, show error message
-                    Toast.makeText(MainActivity.this, "Invalid Input", Toast.LENGTH_SHORT).show();
-                } else {
-                    convert();
-                }
+            if (inputText.isEmpty() || !isValidDouble(inputText)) {
+                Toast.makeText(MainActivity.this, "Invalid Input", Toast.LENGTH_SHORT).show();
+            } else {
+                convert();
             }
         });
     }
-    // Method to check if a string represents a valid positive double
+
+    private void updateUnitSpinners(String category) {
+        int unitArrayResourceId = R.array.length_units;
+        if (category.equals("Weight")) {
+            unitArrayResourceId = R.array.weight_units;
+        } else if (category.equals("Temperature")) {
+            unitArrayResourceId = R.array.temperature_units;
+        }
+
+        ArrayAdapter<CharSequence> unitAdapter = ArrayAdapter.createFromResource(this,
+                unitArrayResourceId, android.R.layout.simple_spinner_item);
+        unitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sourceUnitSpinner.setAdapter(unitAdapter);
+        destinationUnitSpinner.setAdapter(unitAdapter);
+    }
+
     private boolean isValidDouble(String string) {
         try {
-            // Try to parse the string as a double
             double value = Double.parseDouble(string);
-            // Check if the parsed value is positive
             return value > 0;
         } catch (NumberFormatException e) {
-            // If parsing fails, catch the exception and return false
             return false;
         }
     }
 
-    // Method to perform conversion
     private void convert() {
-        String type = typeSpinner.getSelectedItem().toString();
+        String category = categorySpinner.getSelectedItem().toString();
         String sourceUnit = sourceUnitSpinner.getSelectedItem().toString();
         String destinationUnit = destinationUnitSpinner.getSelectedItem().toString();
 
-        double inputValue = Double.parseDouble(sourceInput.getText().toString());
-        double resultValue = convertValue(inputValue, sourceUnit, destinationUnit, type);
+        double inputValue = Double.parseDouble(inputValueEditText.getText().toString());
+        double resultValue = convertValue(inputValue, sourceUnit, destinationUnit, category);
         resultTextView.setText(formatResult(inputValue, sourceUnit, resultValue, destinationUnit));
     }
 
-    // Method to perform specific conversion based on type
-    private double convertValue(double inputValue, String sourceUnit, String destinationUnit, String type) {
+    private double convertValue(double inputValue, String sourceUnit, String destinationUnit, String category) {
         double resultValue = 0;
 
-        switch (type) {
+        switch (category) {
             case "Length":
                 resultValue = convertLength(inputValue, sourceUnit, destinationUnit);
                 break;
@@ -154,12 +112,9 @@ public class MainActivity extends AppCompatActivity {
         return resultValue;
     }
 
-    // Method to convert length
     private double convertLength(double value, String sourceUnit, String destinationUnit) {
         double resultValue = 0;
-
-        // Length conversion logic
-        // Switch cases based on source unit
+    
         switch (sourceUnit) {
             case "inch":
                 switch (destinationUnit) {
@@ -296,13 +251,11 @@ public class MainActivity extends AppCompatActivity {
         }
         return resultValue;
     }
+    
 
-    // Method to convert weight
     private double convertWeight(double value, String sourceUnit, String destinationUnit) {
         double resultValue = 0;
-
-        // Weight conversion logic
-        // Handle cases for sourceUnit and destinationUnit
+    
         switch (sourceUnit) {
             case "pound":
                 switch (destinationUnit) {
@@ -310,89 +263,89 @@ public class MainActivity extends AppCompatActivity {
                         resultValue = value;
                         break;
                     case "ounce":
-                        resultValue = value * 16; // 1 pound = 16 ounces
+                        resultValue = value * 16;
                         break;
                     case "ton":
-                        resultValue = value / 2000; // 1 pound = 0.0005 ton
+                        resultValue = value / 2000;
                         break;
                     case "kg":
-                        resultValue = value * 0.453592; // 1 pound = 0.453592 kg
+                        resultValue = value * 0.453592;
                         break;
                     case "g":
-                        resultValue = value * 453.592; // 1 pound = 453.592 g
+                        resultValue = value * 453.592;
                         break;
                 }
                 break;
             case "ounce":
                 switch (destinationUnit) {
                     case "pound":
-                        resultValue = value / 16; // 1 ounce = 1/16 pounds
+                        resultValue = value / 16;
                         break;
                     case "ounce":
                         resultValue = value;
                         break;
                     case "ton":
-                        resultValue = value / 32000; // 1 ounce = 0.00003125 ton
+                        resultValue = value / 32000;
                         break;
                     case "kg":
-                        resultValue = value * 0.0283495; // 1 ounce = 0.0283495 kg
+                        resultValue = value * 0.0283495;
                         break;
                     case "g":
-                        resultValue = value * 28.3495; // 1 ounce = 28.3495 g
+                        resultValue = value * 28.3495;
                         break;
                 }
                 break;
             case "ton":
                 switch (destinationUnit) {
                     case "pound":
-                        resultValue = value * 2000; // 1 ton = 2000 pounds
+                        resultValue = value * 2000;
                         break;
                     case "ounce":
-                        resultValue = value * 32000; // 1 ton = 32000 ounces
+                        resultValue = value * 32000;
                         break;
                     case "ton":
                         resultValue = value;
                         break;
                     case "kg":
-                        resultValue = value * 907.185; // 1 ton = 907.185 kg
+                        resultValue = value * 907.185;
                         break;
                     case "g":
-                        resultValue = value * 907185; // 1 ton = 907185 g
+                        resultValue = value * 907185;
                         break;
                 }
                 break;
             case "kg":
                 switch (destinationUnit) {
                     case "pound":
-                        resultValue = value / 0.453592; // 1 kg = 2.20462 pounds
+                        resultValue = value / 0.453592;
                         break;
                     case "ounce":
-                        resultValue = value / 0.0283495; // 1 kg = 35.274 ounces
+                        resultValue = value / 0.0283495;
                         break;
                     case "ton":
-                        resultValue = value / 907.185; // 1 kg = 0.00110231 tons
+                        resultValue = value / 907.185;
                         break;
                     case "kg":
                         resultValue = value;
                         break;
                     case "g":
-                        resultValue = value * 1000; // 1 kg = 1000 g
+                        resultValue = value * 1000;
                         break;
                 }
                 break;
             case "g":
                 switch (destinationUnit) {
                     case "pound":
-                        resultValue = value / 453.592; // 1 g = 0.00220462 pounds
+                        resultValue = value / 453.592;
                         break;
                     case "ounce":
-                        resultValue = value / 28.3495; // 1 g = 0.035274 ounces
+                        resultValue = value / 28.3495;
                         break;
                     case "ton":
-                        resultValue = value / 907185; // 1 g = 0.00000110231 tons
+                        resultValue = value / 907185;
                         break;
                     case "kg":
-                        resultValue = value / 1000; // 1 g = 0.001 kg
+                        resultValue = value / 1000;
                         break;
                     case "g":
                         resultValue = value;
@@ -402,11 +355,11 @@ public class MainActivity extends AppCompatActivity {
         }
         return resultValue;
     }
+    
 
-    // Method to convert weight
     private double convertTemperature(double value, String sourceUnit, String destinationUnit) {
         double resultValue = 0;
-
+    
         switch (sourceUnit) {
             case "Celsius":
                 switch (destinationUnit) {
@@ -414,33 +367,33 @@ public class MainActivity extends AppCompatActivity {
                         resultValue = value;
                         break;
                     case "Fahrenheit":
-                        resultValue = (value * 9 / 5) + 32; // Celsius to Fahrenheit conversion formula
+                        resultValue = (value * 1.8) + 32;
                         break;
                     case "Kelvin":
-                        resultValue = value + 273.15; // Celsius to Kelvin conversion formula
+                        resultValue = value + 273.15;
                         break;
                 }
                 break;
             case "Fahrenheit":
                 switch (destinationUnit) {
                     case "Celsius":
-                        resultValue = (value - 32) * 5 / 9; // Fahrenheit to Celsius conversion formula
+                        resultValue = (value - 32) / 1.8;
                         break;
                     case "Fahrenheit":
                         resultValue = value;
                         break;
                     case "Kelvin":
-                        resultValue = (value - 32) * 5 / 9 + 273.15; // Fahrenheit to Kelvin conversion formula
+                        resultValue = (value - 32) / 1.8 + 273.15;
                         break;
                 }
                 break;
             case "Kelvin":
                 switch (destinationUnit) {
                     case "Celsius":
-                        resultValue = value - 273.15; // Kelvin to Celsius conversion formula
+                        resultValue = value - 273.15;
                         break;
                     case "Fahrenheit":
-                        resultValue = (value - 273.15) * 9 / 5 + 32; // Kelvin to Fahrenheit conversion formula
+                        resultValue = (value - 273.15) * 1.8 + 32;
                         break;
                     case "Kelvin":
                         resultValue = value;
@@ -448,22 +401,13 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
         }
-
+    
         return resultValue;
     }
+    
 
-    //formatting result
     private String formatResult(double inputValue, String sourceUnit, double resultValue, String destinationUnit) {
         DecimalFormat df = new DecimalFormat("#.####");
         return "Result: " + inputValue + " " + sourceUnit + " = " + df.format(resultValue) + " " + destinationUnit;
     }
 }
-
-
-
-
-
-
-
-
-
